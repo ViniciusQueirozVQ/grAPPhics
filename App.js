dc14fa5773dcf70cfa75ade87659f565b5d44612
ViniciusQueirozVQ <vinciusqueiroz.voq@gmail.com>
2022-05-18 20:32:02 -0300

    Update Versão com navegação inicial

--- App.js
+++ App.js
@@ -1,33 +1,58 @@
 import React from "react";
 
-import { StatusBar, Text} from "react-native";
+import { StatusBar, Text, View, Button } from "react-native";
 import { MenuProvider } from "react-native-popup-menu";
-
-
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Feeds from "./src/screens/Feeds";
 import Detalhes from "./src/screens/Detalhes";
 
-// const Navegador = createStackNavigator(
-//   {
-//     Feeds: { screen: Feeds },
-//     Detalhes: { screen: Detalhes }
-//   },
-//   {
-//     headerMode: "none"
-//   }
-// );
+const Stack = createStackNavigator();
 
-// const Contentor = createAppContainer(Navegador)
+function TelaHome({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tela Canal</Text>
+      <Text>Youtube Tela</Text>
+
+      <Button
+        title="Detalhes"
+        onPress={() => navigation.navigate('Detalhes')}
+      />
+
+      <Button
+        title="Feeds"
+        onPress={() => navigation.navigate('Feeds')}
+      />
+
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <MenuProvider>
       <StatusBar barStyle="dark-content" backgroundColor={"#fff"} />
-      <Feeds> Oi Aqui é a tela Inicial </Feeds>
-      <Text> Aqui temos que mostrar os feeds</Text>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={TelaHome}
+            options={{ title: 'Tela de Home' }}
+          />
+          <Stack.Screen
+            name="Feeds"
+            component={Feeds}
+            options={{ title: 'Tela de Feeds' }}
+          />
+          <Stack.Screen
+            name="Detalhes"
+            component={Detalhes}
+            options={{ title: 'Tela de Detalhes da GPU' }}
+          />
+
+        </Stack.Navigator>
+      </NavigationContainer>
     </MenuProvider>
   )
 }
--- src/screens/Feeds/index.js
+++ src/screens/Feeds/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
+import bancoEstatico from "../../assets/feeds.json";
 
 export default class Feeds extends React.Component {
 
@@ -11,9 +12,9 @@ export default class Feeds extends React.Component {
         return (
             <View>
                 <Text>
-                OI Sou a tela de Feeds
+                    OI Sou a tela de Feeds
                 </Text>
             </View>
         );
-      }
+    }
 }
